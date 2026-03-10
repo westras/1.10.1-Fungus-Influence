5448fedf54576b131bacc7835a544e8bc8dd33e4
westras <r.westholm@telia.com>
2026-03-10 14:09:33 +0100

    now carruslesle worksihs

---
+++ char.js
@@ -0,0 +1,16 @@
+    function changeImage(img, character) {
+    const mainImg = document.querySelector(`.character-card .left img.main-img`);
+    mainImg.src = img.src;
+  }
+
+  function openLightbox(character) {
+    const mainImg = document.querySelector(`.character-card .left img.main-img`);
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = mainImg.src;
+    lightbox.style.display = 'flex';
+  }
+
+  function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+  }
