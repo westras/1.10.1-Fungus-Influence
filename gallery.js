5ac2ada8e3dbcf95b784906494d5626a89a19a2e
westras <r.westholm@telia.com>
2026-03-13 10:26:15 +0100

    start gallery

---
+++ gallery.js
@@ -0,0 +1,11 @@
+function filterGallery(type){
+const items=document.querySelectorAll(".item");
+
+items.forEach(img=>{
+if(type==="all" || img.classList.contains(type)){
+img.style.display="block";
+}else{
+img.style.display="none";
+}
+});
+}
