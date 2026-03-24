01cb9898a08932da6e2d4e52513934e49427debe
westras <r.westholm@telia.com>
2026-03-24 13:51:49 +0100

    w

--- Quiz.js
+++ Quiz.js
@@ -1,9 +1,9 @@
 function result(character){
 
     const results={
-    ellie:"You are Ellie — brave, smart, and determined.",
-    joel:"You are Joel — protective and strong.",
-    abby:"You are Abby — powerful and relentless."
+    ellie:"You are Ellie, brave, smart, and determined.",
+    joel:"You are Joel, protective and strong.",
+    abby:"You are Abby, powerful and relentless."
     }
 
     const resultBox = document.getElementById("result");
